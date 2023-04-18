5bbf929a5df4ca3b81b2e20bcd5f5c32e3f36f78
LBeul <l.beul@outlook.de>
2023-04-18 12:57:42 +0200

    build tooltip opover button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
-import PopOver from './components/PopOver';
 import TabContainer from './components/TabContainer';
-import ToolTip from './components/ToolTip';
-
-const dummyText =
-  'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut sunt debitis, optio recusandae minus et culpa enim veritatis, provident animi autem inventore velit sed officia architecto, eaque tempore perspiciatis quas.';
 
 function App() {
   return (
@@ -12,8 +7,6 @@ function App() {
         Einstellungen für DigitZ %NetworkName%
       </h2>
       <TabContainer />
-      <ToolTip id='test' label='?' content={dummyText} />
-      <PopOver />
     </div>
   );
 }
--- src/components/ToolTip.jsx
+++ src/components/ToolTip.jsx
@@ -1,17 +1,31 @@
 import { createSignal } from 'solid-js';
 
-const ToolTip = ({ label, content, id }) => {
-  const [showPopOver, setShowPopOver] = createSignal(false);
+/**
+ * A "?"-Button that renders a popover on click
+ * Used to give further information and instructions throughout the app
+ * @param {content} : the content to be displayed inside the tooltip popover
+ * @returns a tooltip button that renders a content onClick
+ */
+const ToolTip = ({ content }) => {
+  const [isHidden, setIsHidden] = createSignal(true);
   return (
-    <button
-      class='text-white bg-emerald-300 font-medium rounded-full text-sm w-5 h-5 text-center'
-      type='button'
-      onClick={() => {
-        alert({ content });
-      }}
-    >
-      {label}
-    </button>
+    <>
+      <button
+        type='button'
+        class='text-white bg-emerald-300 font-medium rounded-full text-sm w-5 h-5 text-center'
+        onclick={() => {
+          setIsHidden(!isHidden());
+        }}
+      >
+        ?
+      </button>
+      <div
+        class='absolute w-max whitespace-normal break-words rounded-lg border border-emerald-gray-50 bg-white p-4 font-sans text-sm font-normal text-emerald-gray-500 shadow-lg shadow-emerald-gray-500/10 focus:outline-none'
+        hidden={isHidden()}
+      >
+        {content}
+      </div>
+    </>
   );
 };
 
