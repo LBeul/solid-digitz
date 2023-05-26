d285f6af5976ef919fbd15aa9d816cc99e1d8735
LBeul <l.beul@outlook.de>
2023-05-26 07:29:47 +0200

    remove darkmode config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
   content: [],
   theme: {
     container: {
